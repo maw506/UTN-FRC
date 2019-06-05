af26ba3a5b58d59831f25b1cf7ad4a1862244102
maw506 <gigenamauro@gmail.com>
2019-06-04 23:14:06 -0300

    menu de opciones

--- TP2.py
+++ TP2.py
@@ -7,4 +7,23 @@ from datetime import datetime
     *tiempo = instanteFinal - instanteInicial # Devuelve un objeto timedelta
     *segundos = tiempo.seconds """
 
+instanteInicial = datetime.now()
+op = 0
+while op != 3:
+    print("\nOPCIONES: \n1 = Para generar datos de forma automatica \n2 = Para ingresar datos de forma manual \n3 = Salir")
+    op = int(input("\nIngrese una opcion: "))
+    if op == 1:
+        print("eligio 1")
+    elif op == 2:
+        print("eligio 2")
+    elif op == 3:
+        print("salir")
+        break
+    else:
+        print("ERROR! opcion seleccionada no valida \nPorfavor elija de nuevo:")
 
+instanteFinal = datetime.now()
+tiempo = instanteFinal - instanteInicial # Devuelve un objeto timedelta
+segundos = tiempo.seconds
+
+print("segundos:", segundos)
