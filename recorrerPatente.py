1f56b169301bf54e85faf65a1c71e62243b81def
maw506 <gigenamauro@gmail.com>
2019-06-07 21:01:34 -0300

    validad patente

--- recorrerPatente.py
+++ recorrerPatente.py
@@ -1,20 +1,39 @@
-patente1 = "ABC123"
-patente2 = "AB123CD"
+patente2 = "AB123CD."
+patente1 = "ABC123."
 
-letras = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-numeros = '0123456789'
 
-contL = 0
-contN = 0
-caracteres = 0
+def validarPatente(patente):
 
-for i in patente2:
-    caracteres += 1
-    if i in letras:
-        contL += 1
-        if contL > 3:
-            print("error de patente")
-    elif i in numeros:
+    letras = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    numeros = '0123456789'
+
+    contL = 0
+    contN = 0
+    caracteres = 0
+
+    long = len(patente)
+
+
+
+    if long == 6:
+        for i in patente:
+            caracteres += 1
+            if i in letras:
+                contL += 1
+                if contL > 3:
+                    msj = "error patente ingresada"
+            elif i in numeros:
+                contN += 1
+                if contN > 3:
+                    msj = "error patente ignresada"
+
+    elif long == 7:
         pass
+    else:
+        msj = "Error, patente ingresada incorrecta"
 
-    print(caracteres)
+    return msj
+
+
+validarPatente(patente1)
+
